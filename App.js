4d569b6545c8e2b169491177c826122e4dd3394e
Manoj <manoj.delhi24@gmail.com>
2019-04-25 22:19:54 +0700

    initialising firebase.

--- App.js
+++ App.js
@@ -3,15 +3,30 @@ import { Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import Config from 'react-native-config';
+import firebase from 'firebase';
 import reducers from './src/reducers';
 
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    const config = {
+      apiKey: Config.ApiKey,
+      authDomain: Config.AuthDomain,
+      databaseURL: Config.DatabaseURL,
+      projectId: Config.ProjectId,
+      storageBucket: Config.StorageBucket,
+      messagingSenderId: Config.MessagingSenderId
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
         <View>
-          <Text>Welcome to React Native! {Config.API_URL }</Text>
+          <Text>Welcome to React Native!</Text>
+          <Text>{Config.DatabaseURL}</Text>
         </View>
       </Provider>
     );
