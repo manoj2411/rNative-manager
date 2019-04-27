aebe1f2f480e65fc2c397c1c2ea9a5b9cbef556d
Manoj <manoj.delhi24@gmail.com>
2019-04-27 17:55:47 +0700

    Refactoring action creator: creating separate handlers to dispatch login success and failure actions.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase';
-import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_SUCCESS } from './types';
+import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_SUCCESS, LOGIN_FAIL } from './types';
 
 
 export const emailChanged = (text) => {
@@ -19,8 +19,20 @@ export const passwordChanged = (text) => {
 export const loginUser = ({ email, password }) => {
   return (dispatch) => {
     firebase.auth().signInWithEmailAndPassword(email, password)
-      .then(user => {
-        dispatch({ type: LOGIN_SUCCESS, payload: user });
+      .then(user => handleLoginSuccess(dispatch, user))
+      .catch(err => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .then(user => handleLoginSuccess(dispatch, user))
+          .catch(error => handleLoginFailure(dispatch, err));
       });
   };
+};
+
+
+const handleLoginSuccess = (dispatch, user) => {
+  dispatch({ type: LOGIN_SUCCESS, payload: user });
+};
+
+const handleLoginFailure = (dispatch, error) => {
+  dispatch({ type: LOGIN_FAIL });
 };
